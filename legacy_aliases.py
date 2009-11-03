cf9765d50b93d1171b285e02a8a1c622e8e47191
Ben Cherry <bcherry@gmail.com>
2009-11-02 23:34:05 -0800

    added legacy redirects for old blog entries

--- legacy_aliases.py
+++ legacy_aliases.py
@@ -7,4 +7,11 @@
 # (without host uri stem) and the permalink as the value.
 
 redirects = {
+	"archives/147"	: "http://bcherry-blog.appspot.com/2009/11/JS-Find-and-Replace-with-SplitJoin",
+	"archives/140"	: "http://bcherry-blog.appspot.com/2009/11/Managing-CSS-Through-Javascript",
+	"archives/134"	: "http://bcherry-blog.appspot.com/2009/11/SimpleThreading-JavaScript-Pseudo-threads",
+	"archives/121"	: "http://bcherry-blog.appspot.com/2009/11/Javascript-Pseudo-threading",
+	"archives/112"	: "http://bcherry-blog.appspot.com/2009/11/Building-a-Better-Friend-Selector",
+	"archives/97"	: "http://bcherry-blog.appspot.com/2009/11/jQuery-Micro-templates",
+	"archives/89"	: "http://bcherry-blog.appspot.com/2009/11/Doing-Javascript-Right",
 }
