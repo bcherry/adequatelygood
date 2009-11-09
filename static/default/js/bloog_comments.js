160ef202a378c6cdc52182b1bec735db2ab74f74
Ben Cherry <bcherry@gmail.com>
2009-11-08 23:39:39 -0800

    began a transition to jquery ui for comments

--- static/default/js/bloog_comments.js
+++ static/default/js/bloog_comments.js
@@ -1,27 +1,88 @@
-/**
- The MIT License
- 
- Copyright (c) 2008 William T. Katz
- 
- Permission is hereby granted, free of charge, to any person obtaining a copy
- of this software and associated documentation files (the "Software"), to 
- deal in the Software without restriction, including without limitation 
- the rights to use, copy, modify, merge, publish, distribute, sublicense, 
- and/or sell copies of the Software, and to permit persons to whom the 
- Software is furnished to do so, subject to the following conditions:
- 
- The above copyright notice and this permission notice shall be included in
- all copies or substantial portions of the Software.
- 
- THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
- IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
- FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
- AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
- LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING
- FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER 
- DEALINGS IN THE SOFTWARE.
-**/
+;(function($){
+	var agd = AGD.extend("Comments");
+	var dialog;
+	var editor;
+	var href;
+	var inputs = {};
 
+	var dialogOnClose = function() {
+		agd.log("dialog closed");
+	};
+
+	var dialogOnOpen = function() {
+		agd.log("dialog opened");
+	};
+
+	var submitComment = function() {
+		agd.log("comment submission begun, submitting to %s", href);
+		$.post(href, {
+			key		: href,
+			captcha	: inputs.captcha.val(),
+			name	: inputs.name.val(),
+			email	: inputs.email.val(),
+			homepage: inputs.homepage.val(),
+			title	: inputs.title.val(),
+			body	: editor.getData()
+		}, function(data) {
+			closeDialog();
+		}, "html");
+	};
+
+	var closeDialog = function() {
+		dialog.dialog('close');
+		href = null;
+	};
+
+	var dialogConfig = {
+		autoOpen	: false,
+		modal		: true,
+		title		: "Leave a Comment!",
+		width		: 500,
+		close		: dialogOnClose,
+		open		: dialogOnOpen,
+		buttons		: {
+			"Leave Comment"	: submitComment,
+			"Cancel"		: closeDialog
+		}
+	};
+
+	var editorConfig = {
+		toolbar		: "Basic"
+	};
+
+	var resetDialog = function() {
+		inputs.captcha.val("");
+		inputs.name.val("");
+		inputs.email.val("");
+		inputs.homepage.val("");
+		inputs.title.val("");
+		inputs.body.val("");
+		editor.setData("");
+	};
+
+	var openDialog = function() {
+		resetDialog();
+		href = $(this).get(0).href;
+		dialog.dialog('open');
+	};
+
+	var initComments = function() {
+		dialog = $("#commentDialog").dialog(dialogConfig);
+		editor = CKEDITOR.replace(dialog.find("#commentBody").get(0), editorConfig);
+
+		inputs.captcha = dialog.find("#captcha");
+		inputs.name = dialog.find("#commentName");
+		inputs.email = dialog.find("#commentEmail");
+		inputs.homepage = dialog.find("#commentHomepage");
+		inputs.title = dialog.find("#commentTitle");
+		inputs.body = dialog.find("#commentBody");
+	};
+
+	$(function(){
+		initComments();
+		$(".replybtn").live("click", openDialog);
+	});
+})(jQuery);
 YAHOO.bloog.initComments = function() {
 
     var showRTE = function(e) {
@@ -148,4 +209,4 @@ YAHOO.bloog.initComments = function() {
     }))
 }
 
-YAHOO.util.Event.onDOMReady(YAHOO.bloog.initComments);
+//YAHOO.util.Event.onDOMReady(YAHOO.bloog.initComments);
