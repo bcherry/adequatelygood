90436b8c2332bb2f29777dad150a4548fd175a81
Ben Cherry <bcherry@gmail.com>
2009-11-21 19:05:20 -0800

    more design work

--- static/js/ag.js
+++ static/js/ag.js
@@ -1,10 +1,22 @@
 ;(function($){
 	$(function() {
-	//	$(".nav span").each(function(){$(this).text("test text")}).show();
+		
+		// Nav bar links
 		$(".nav a").hover(function() {
 			$(this).siblings("span").fadeIn("fast");
 		}, function() {
 			$(this).siblings("span").fadeOut("fast");
+		});
+		
+		// Search
+		$("#search input[type=text]").focus(function() {
+			if ($(this).val() == $(this).attr("title")) {
+				$(this).removeClass("blurred").val("");
+			}
+		}).blur(function() {
+			if (!$(this).val()) {
+				$(this).addClass("blurred").val($(this).attr("title"));
+			}
 		});
 	});
 })(jQuery);
